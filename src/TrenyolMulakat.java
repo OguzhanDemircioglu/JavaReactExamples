63b7168ad0b0314cbf4fd96cd36827657d7df48a
Oğuzhan Turgut Demircioğlu (ETIYA) <oguzhan.demircioglu@etiya.com>
2023-08-16 14:50:11 +0300

    yorum

--- src/TrenyolMulakat.java
+++ src/TrenyolMulakat.java
@@ -5,6 +5,12 @@ public class TrenyolMulakat {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        /*
+        Girilen Sayinin basamaklari toplanir, girilen sayiyi bir arttirarak gelen sayinin basamaklari toplami ile kiyaslanır,
+        girilen deger ve siradaki degerin basamak toplamları eşit ise, gelen ardisik sayi degeri donulur
+        Ornek:28 ise 37 , 734 ise 743, 1000 ise 10000
+         */
+
         System.out.println("Sayi giriniz: ");
         String N = scan.nextLine();
 
