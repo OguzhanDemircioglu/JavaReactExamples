24ea9441a3012b7ca455bf8834b260bbb6feb402
cray61 <oguzhanturgut611@gmail.com>
2024-01-02 01:06:00 +0300

    react-first

--- src/MovingTotal.java
+++ src/MovingTotal.java
@@ -1,28 +1,27 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MovingTotal {
     /**
      * Adds/appends list of integers at the end of internal list.
      */
-    List<Integer> sumList = new ArrayList<Integer>();
+    List<Integer> sumList = new ArrayList<>();
+    List<Integer> valList = new ArrayList<>();
 
 
     public void append(int[] list) {
-        sumList.add(Arrays.stream(list).reduce((i, sum) -> sum = i + sum).getAsInt());
+        int lastIndex = sumList.isEmpty() ? -1 : sumList.size() - 1;
+
+        IntStream.range(0, list.length)
+                .forEach(i -> sumList.add(list[i] + (i > 0 ? list[i - 1] : 0) + (i > 1 ? list[i - 2] : 0)));
     }
 
-    /**
-     * Returns boolean representing if any three consecutive integers in the
-     * internal list have given total.
-     */
     public boolean contains(int total) {
-        if (sumList.contains(total)) {
-            return true;
-        } else {
-            return false;
-        }
+        return sumList.contains(total);
     }
 
     public static void main(String[] args) {
@@ -30,16 +29,20 @@ public class MovingTotal {
 
         movingTotal.append(new int[]{1, 2, 3, 4});
 
-        System.out.println(movingTotal.contains(6));
-        System.out.println(movingTotal.contains(9));
-        System.out.println(movingTotal.contains(12));
-        System.out.println(movingTotal.contains(7));
+        System.out.println(movingTotal.contains(6));  // true
+        System.out.println(movingTotal.contains(9));  // true
+        System.out.println(movingTotal.contains(12)); // false
+        System.out.println(movingTotal.contains(7));  // true
 
         movingTotal.append(new int[]{5});
+        System.out.println("---");
+        System.out.println(movingTotal.contains(6));  // true
+        System.out.println(movingTotal.contains(9));  // true
+        System.out.println(movingTotal.contains(12)); // true
+        System.out.println(movingTotal.contains(7));  // true
 
-        System.out.println(movingTotal.contains(6));
-        System.out.println(movingTotal.contains(9));
-        System.out.println(movingTotal.contains(12));
-        System.out.println(movingTotal.contains(7));
+        for (int i : movingTotal.sumList) {
+            System.out.println(i);
+        }
     }
 }
